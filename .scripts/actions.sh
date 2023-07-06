2c04a53799cab72292d81eb40e7f69757cfac23b
shihabsararrafid <shrafid.532@gmail.com>
2023-07-06 17:03:18 +0600

    d

--- .scripts/actions.sh
+++ .scripts/actions.sh
@@ -4,11 +4,11 @@ set -e
 echo "Deployment started..."
 
 # Pull the latest version of the app
-git pull origin main
+# git pull origin main
 echo "New changes copied to server !"
 
 echo "Installing Dependencies..."
-npm install --yes
+# npm install --yes
 
 
 
