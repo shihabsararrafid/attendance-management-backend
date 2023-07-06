423397881c1f86a7a98a226148116f68d7a14581
shihabsararrafid <shrafid.532@gmail.com>
2023-07-06 16:40:13 +0600

    action added

---
+++ .scripts/actions.sh
@@ -0,0 +1,15 @@
+
+set -e
+
+echo "Deployment started..."
+
+# Pull the latest version of the app
+git pull origin main
+echo "New changes copied to server !"
+
+echo "Installing Dependencies..."
+npm install --yes
+
+
+
+echo "Deployment Finished!"
