984d2f265126700c35684b787892841c7d6d288e
shihabsararrafid <shrafid.532@gmail.com>
2023-09-16 10:40:40 +0600

    [done] resolved the route

--- src/module/students/students.service.js
+++ src/module/students/students.service.js
@@ -3,7 +3,7 @@ const Class = require("../teachers/Models/class.model");
 module.exports.getStudentsAllClassService = async (id) => {
   const classes = await Class.find(
     { "students.userId": id },
-    { _id: 1, code: 1 }
+    { _id: 1, code: 1, section: 1, batchName: 1 }
   );
   // const classCode =
   return classes;
